d178fc2a6f41f829e5ce0077e104faa52c912637
Robin Ricard <ricard.robin@gmail.com>
2016-02-11 20:42:28 +0100

    may work

--- nearest-order.js
+++ nearest-order.js
@@ -30,7 +30,8 @@ function initDrones(nd, initW) {
     c: initW.c,
     idle: 0,
     products: [],
-    nextOrder: null
+    nextOrder: null,
+    nextOrderI: null
   });
 }
 
@@ -41,44 +42,62 @@ module.exports = function(map, warehouses, productTypes, orders) {
   let drones = initDrones(map.nd, warehouse[0]);
   let commands = [];
   while(orders.length > 0 && waiting < MAX_WAITING) {
-    drones.forEach(drone => {
+    drones.forEach((drone, di) => {
       // Case #1: Idle drone without an order (=> MUST LOAD, CHOOSE AN O&W)
       if(drone.idle < 1 && !drone.nextOrder) {
         // 1. Choose the best warehouse
         let chosenW = null;
+        let chosenWi = null;
         let chosenWScore = 0;
-        warehouses.forEach(w => {
+        warehouses.forEach((w, wi) => {
           const pScore = w.quantities.reduce((q, type) => scoreQuantity(productTypes[type], q));
           const wScore = betterWarehouse(pScore, euclideanDist(w, drone));
           if(wScore > chosenWScore) {
             chosenWScore = wScore;
             chosenW = w;
+            chosenWi = wi;
           }
         });
         // 2. Choose the best order from this warehouse
         let chosenO = null;
+        let chosenOi = null;
         let chosenOScore = 0;
-        orders.filter(o => {
-          return o.quantities.filter((q, type) => q > chosenW.quantities[type]).length < 1;
+        orders.filter((o, oi) => {
+          return !!o && o.quantities.filter((q, type) => q > chosenW.quantities[type]).length < 1;
         }).forEach(o => {
           const pScore = o.quantities.reduce((q, type) => scoreQuantity(productTypes[type], q));
           const oScore = betterOrder(pScore, euclideanDist(o, chosenW));
           if(oScore > chosenOScore) {
             chosenOScore = oScore;
             chosenO = o;
+            chosenOi = oi;
           }
         });
-        drone.order = chosenO;
-        if(drone.order) {
-          // TODO: launch a LOAD action
+        drone.nextOrder = chosenO;
+        drone.nextOrderI = chosenOi;
+        if(drone.nextOrder) {
+          chosenO.forEach((q, type) => {
+            commands.push({d: di, type: "L", args: [chosenWi, type, q]});
+            chosenW.quantities[type] -= q;
+            drone.idle += 1;
+          });
+          drone.idle += euclideanDist(o, chosenW);
         } else {
-          // TODO: WAIT
+          commands.push({d: di, type: "W", args: [1]});
+          drone.idle += 1;
           waiting += 1;
         }
       }
       // Case #2: Idle drone with an order, AUTO
       else if(drone.idle < 1 && drone.nextOrder) {
-        // TODO: launch a DELIVER action
+        orders[drone.nextOrderI] = null;
+        chosenO.forEach((q, type) => {
+          commands.push({d: di, type: "D", args: [drone.nextOrderI, type, q]});
+          drone.idle += 1;
+        });
+        drone.idle += euclideanDist(drone, drone.nextOrder);
+        drone.nextOrder = null;
+        drone.nextOrderI = null;
       }
       // Case #3: Not idle, AUTO
       else {
