8a3291a906780dc5081868d312b70726298b2316
Edouard Oger <eoger@fastmail.com>
2016-02-11 20:52:53 +0100

    export commands total

--- export-solution.js
+++ export-solution.js
@@ -1,6 +1,7 @@
 "use strict";
 
 module.exports = function (commands) {
+  console.log(commands.length);
   for(let command of commands) {
     console.log(command.drone + " " + command.type + " " + command.args.join(" "));
   }
